363c685e9f8bc7487221bf097978817816f541d6
Tibor Meller <tmeller@cloudera.com>
2024-03-12 16:24:28 +0100

    Nesting more describe/it

--- components/PseudoComponent/PseudoComponent.spec.ts
+++ components/PseudoComponent/PseudoComponent.spec.ts
@@ -1,9 +1,25 @@
 import PseudoComponent from "../PseudoComponent/PseudoComponent";
 import { describe, expect, it } from "../../utils/test-lib/test-lib";
 
-describe("Testing PseudoComponent", () => {
-    describe("  Testing sum function", () => {
-        it("Should propery sum number with sum()", () => {
+describe("Testing PseudoComponent #1", () => {
+    describe("Testing sum function #1@1", () => {
+        it("Should propery sum number with sum() #1#1", () => {
+            const what = new PseudoComponent();
+
+            expect(what.summ(1, 2)).toBe(4);
+        });
+
+        describe("Testing sum function #1@2@1", () => {
+            it("Should propery sum number with sum() #1#2", () => {
+                const what = new PseudoComponent();
+    
+                expect(what.summ(1, 2)).toBe(3);
+            });
+        });
+    });
+
+    describe("Test after deep nesting #1@3", () => {
+        it("Should propery sum number with sum() #1#3", () => {
             const what = new PseudoComponent();
 
             expect(what.summ(1, 2)).toBe(4);
--- components/TSXPseudoComponent/TSXPseudoComponent.spec.ts
+++ components/TSXPseudoComponent/TSXPseudoComponent.spec.ts
@@ -1,8 +1,14 @@
 import TSXPsuedoComponent from './TSXPsuedoComponent';
 import { describe, expect, it } from '../../utils/test-lib/test-lib';
 
-describe("Testing pseudo TSX React component", () => {
-    it("Should return with a testable DOM", () => {
+describe("Testing pseudo TSX React component #2", () => {
+    it("Should return with a testable DOM #2#1", () => {
+        const what = TSXPsuedoComponent({ name: "Hello" });
+
+        expect(what.type).toBe("div");
+    });
+
+    it("Should return with a testable DOM #2#2", () => {
         const what = TSXPsuedoComponent({ name: "Hello" });
 
         expect(what.type).toBe("div");
